676179ade62762a342a68d7c5712f65e53a9cf97
ElineQ <84079851+ElineQ@users.noreply.github.com>
2021-06-04 20:57:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let Name = 'Eline'; //string literal
+console.log(Name);
+
+let interestRate = 0,3;
+interestRate = 1;
+console.log(interestRate);
+
+let age = 25; //numer literal
+
+let isApproved = false; //Boolean literal (1 van de 2)
+
+let firstName = undefined;
+
+let selectedColor = null;
